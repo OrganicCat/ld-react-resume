8f1c730d9af60096795b37820ea12fe8dd96237a
Lee Delarm <lee.delarm@gmail.com>
2017-05-01 21:19:38 -0500

    Added better text, transparent overlay for card content

--- src/App.js
+++ src/App.js
@@ -40,42 +40,47 @@ class App extends Component {
     const bodyContent = [
         {
           id: 1,
-          cardHeader: 'HTML',
-          cardContent: 'Wow, I have worked with SO MUCH HTML. I can tag with the best of them.',
+          cardHeader: 'HTML + Sites',
+          cardContent: 'Create custom select boxes, construct templates and layout, design flow underneath the artwork. Created 12+ production sites throughout career',
           image: '/imgs/white_magic_card2.jpg',
+          type: 'Create - Skeleton',
           power: '9/10',
           color: 'white'
         },
         {
           id: 2,
-          cardHeader: 'CSS',
-          cardContent: 'Box models, typography, and implementation are what makes the web go round',
+          cardHeader: 'SCSS/CSS',
+          cardContent: 'Can design intricate layouts and implement them with responsive design (desktop to mobile). Experienced in UX for keeping customers interested and decluttering interfaces',
           image: '/imgs/black_magic_card2.jpg',
+          type: 'Sorcery - Black Magic',
           power: '9/10',
           color: 'black'
         },
         {
           id: 3,
           cardHeader: 'JavaScript',
-          cardContent: 'Object oriented programming, now with skills including more type checking and class syntactical sugar',
+          cardContent: 'Worked with JS creating dynamic sites and single page applications. ES6 ready, but ES5 backwards compatible',
           image: '/imgs/green_magic_card2.jpg',
+          type: 'Summon - Wall',
           power: '8/10',
           color: 'green'
         },
         {
           id: 4,
           cardHeader: 'AngularJS',
-          cardContent: 'A popular flavor of the past few years, I have experience in designing and developing with 1.x',
+          cardContent: 'A popular flavor of the past few years, I have experience in designing and developing 3+ websites with 1.x and one with 2+',
           image: '/imgs/red_magic_card2.jpg',
-          power: '7/10',
+          type: 'Instant - Lighting',
+          power: '6/10',
           color: 'red'
         },
         {
           id: 5,
           cardHeader: 'React',
-          cardContent: 'Guess what this site is built in?',
+          cardContent: 'ReactJS is working well for me, I love the ease of use implementing it on the front end, and have dabbled in backend usage',
           image: '/imgs/blue_magic_card2.jpg',
-          power: '4/10',
+          type: 'Sorcery - Counterspell',
+          power: '3/10',
           color: 'blue'
         }
       ]
